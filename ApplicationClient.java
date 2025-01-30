9a6e4f04c81bc04492ecf8ce617fc96bc54873f8
Dair133 <114807176+Dair133@users.noreply.github.com>
2025-01-30 14:30:25 +0000

    basic client skeleton

---
+++ ApplicationClient.java
@@ -0,0 +1,34 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+
+public class ApplicationClient{
+
+
+
+    private String username = "testUsername";
+    private String password = "testPassword";
+
+    
+    public void downloadForm(){
+
+
+
+    }
+
+    // Methods called which are required to complete the form
+
+
+    public void submitForm(){
+
+
+
+
+
+    }
+
+
+
+
+
+}
