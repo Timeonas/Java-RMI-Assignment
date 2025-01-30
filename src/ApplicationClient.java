83e30e8638152352273e2021c3f2983735b632da
Dair133 <114807176+Dair133@users.noreply.github.com>
2025-01-30 14:52:12 +0000

    client skeleton added back into new structure

---
+++ src/ApplicationClient.java
@@ -0,0 +1,13 @@
+package src;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+public class ApplicationClient{
+    private String username = "testUsername";
+    private String password = "testPassword";
+    
+    public void downloadForm(){
+    }
+    // Methods called which are required to complete the form
+    public void submitForm(){
+    }
+}
